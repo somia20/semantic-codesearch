696bb9186c8dd293b9da223123d9862dbeb0a49a
Somia Kumari <unibicsomia20@gmail.com>
2025-03-28 10:15:32 +0530

    changes done

--- semantics.py
+++ semantics.py
@@ -176,7 +176,7 @@ def search_codebase(client, collection_name: str, query: str, nlp_model, code_mo
         limit=limit
     ).points
 
-    print("Results from code embeddings:")
+    print("Result from code embeddings:")
     for hit in code_hits:
         payload = hit.payload
         print(f"Score: {hit.score:.3f}, Module: {payload['context']['module']}, File: {payload['context']['file_path']}")
